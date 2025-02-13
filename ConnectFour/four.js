dec4946d195a854062a1ea6d86c87a5fcdc2ff62
Kauan Leonel <kauan.br.char@gmail.com>
2025-02-13 15:14:39 -0300

    Personalização do jogo do CurrentFour

--- ConnectFour/four.js
+++ ConnectFour/four.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const squares = document.querySelectorAll('.grid div')
-    const result = document.querySelector('#result')
-    const displayCurrentPlayer = document.querySelector('#current-player')
-    let currentPlayer = 1;
-    const winningArrays = [
+document.addEventListener('DOMContentLoaded', () =>{ //Faz com que o DOM carregue primeiro e não haja confusão no código
+    
+    //VÁRIAVEIS
+    const squares = document.querySelectorAll('.grid div') //Pega todos os quadrados e coloca em um array
+    const result = document.querySelector('#result') // Pega o resultado
+    const displayCurrentPlayer = document.querySelector('#current-player') //Pega o jogador atual
+    let currentPlayer = 1; //Faz com que sempre o player1 comece
+    const winningArrays = [ //Todas as posições vencedoras
         [0, 1, 2, 3], [41, 40, 39, 38],[7, 8, 9, 10],[34, 33, 32, 31],[14, 15, 16, 17],[27, 26, 25, 24],[21, 22, 23, 24],
         [20, 19, 18, 17],[28, 29, 30, 31],[13, 12, 11, 10],[35, 36, 37, 38],[6, 5, 4, 3],[0, 7, 14, 21],[41, 34, 27, 20],
         [1, 8, 15, 22],[40, 33, 26, 19],[2, 9, 16, 23],[39, 32, 25, 18],[3, 10, 17, 24],[38, 31, 24, 17],[4, 11, 18, 25],
@@ -15,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () =>{
         [22, 23, 24, 25],[26, 25, 24, 23],[29, 30, 31, 32],[33, 32, 31, 30],[36, 37, 38, 39],[40, 39, 38, 37],[7, 14, 21, 28],
         [8, 15, 22, 29],[9, 16, 23, 30],[10, 17, 24, 31],[11, 18, 25, 32],[12, 19, 26, 33],[13, 20, 27, 34],
         ]
+
+        //FUNÇÕES
         function checkBoard(){
             for(let y = 0; y < winningArrays.length; y++){
                 const square1 = squares[winningArrays[y][0]];
