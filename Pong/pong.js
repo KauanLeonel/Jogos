bfa88aad3ff8fce8a97a82968b029e94967e787c
Kauan Leonel <kauan.br.char@gmail.com>
2025-02-15 18:02:20 -0300

    Finalização do tutorial do pong com alterações

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,6 +1,41 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 var ctx = canvas.getContext('2d'); //Trabalhar com um jogo em 2d
+canvas.style.background = 'black'
+let dirX = true, dirY = true;
+var pad1YPos, pad2YPos
 var docHeight, docWidth; //setar os tamanhos
+var WKeyState = false
+var SKeyState = false
+var OKeyState = false
+var LKeyState = false
+var Score1 = 0, Score2 = 0
+var requestFrame = false
+
+
+document.addEventListener('keydown', (e) =>{
+    if(e.key == 'w') WKeyState = true;
+    if(e.key == 's') SKeyState = true;
+    if(e.key == "ArrowUp") OKeyState = true;
+    if(e.key == 'ArrowDown') LKeyState = true;
+    if(e.key == 'enter'){
+        if(!requestFrame){
+            var ball = new Obj(docWidth /2, docHeight/2 , 10)
+            ball.desenharBola()
+                requestFrame = true;
+                moverBolaLoop(ball);
+            
+        }
+    }
+})
+
+document.addEventListener('keyup', (e) =>{
+    if(e.key == 'w') WKeyState = false;
+    if(e.key == 's') SKeyState = false;
+    if(e.key == "ArrowUp") OKeyState = false;
+    if(e.key == 'ArrowDown') LKeyState = false;
+})
+
+
 
 function canvasSetup(){
     docHeight = window.innerHeight; //A altura é o tamanho da janela
@@ -9,9 +44,16 @@ function canvasSetup(){
     canvas.width = docWidth;
     pad2Ypos = docHeight/2;
     pad1Ypos = docHeight/2
+    pad1YPos = docHeight / 2;  // Corrigido
+    pad2YPos = docHeight / 2;  // Corrigido
+    dirX = aleatorio()
+    dirY = aleatorio()
     desenharPads(pad1YPos, pad2Ypos);
     var ball = new Obj(docWidth/2, docHeight/2, 10)
     ball.desenharBola();
+    ctx.fillStyle = 'white'
+    ctx.fillRect(docWidth / 2 - 5, 0, 10, docHeight)
+    ctx.fill()
 }
 
 function desenharPads(pad1YPos, pad2YPos){
@@ -32,8 +74,9 @@ class Obj {
     }
 
     drawPad(){
-        ctx.fillRect(this.x, this.y, this.radius, this.height)
         ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.radius, this.height)
+    
     }
 
     desenharBola(){
@@ -43,5 +86,92 @@ class Obj {
         ctx.fill()
         ctx.closePath()
     }
+     moverBola(){
+        ctx.clearRect(0, 0, docWidth + 100, docHeight)
+        desenharPads(pad1YPos, pad2YPos)
+        if(dirY) this.y += this.speed;
+        if(dirX) this.x += this.speed;
+        if(!dirX) this.x -= this.speed;
+        if(!dirY) this.y -= this.speed;
+        ctx.fillStyle = 'white'
+        ctx.fillRect(docWidth/2 - 5, 0 , 10, docHeight)
+        ctx.fill()
+        this.desenharBola()
+        checarColisao(this.y, this.x);
+
+        if(this.y < 0) dirY = true;
+        if(this.y > docHeight) dirY = false;
+
+        if(this.x >docWidth){
+
+            dirX = aleatorio()
+            dirY = aleatorio()
+            this.x = docWidth/2;
+            this.y = docHeight/2;
+            Score1++;
+            requestFrame = false;
+            ctx.clearRect(0, 0, docWidth + 100, docHeight)
+            desenharPads(pad1YPos, pad2YPos)
+            ctx.fillStyle = 'white'
+            ctx.fillRect(docWidth / 2 - 5, 0, 10, docHeight)
+            ctx.fill()
+            this.desenharBola()
+        }
+        if(this.x < 0){
+
+            dirX = aleatorio()
+            dirY = aleatorio()
+            this.x = docWidth/2;
+            this.y = docHeight/2;
+            Score2++;
+            requestFrame = false;
+            ctx.clearRect(0, 0, docWidth + 100, docHeight)
+            desenharPads(pad1YPos, pad2YPos)
+            ctx.fillStyle = 'white'
+            ctx.fillRect(docWidth / 2 - 5, 0, 10, docHeight)
+            ctx.fill()
+            this.desenharBola()
+    }
+    
+}}
+
+function moverBolaLoop(ball){
+    ball.moverBola();
+    if(requestFrame) requestAnimationFrame(() => {moverBolaLoop(ball)})
+
+    if(WKeyState && pad1YPos > 0 ) pad1YPos -= 10;
+    if(SKeyState && pad1YPos < window.innerHeight - 100 ) pad1YPos += 10;
+    if(OKeyState && pad2YPos > 0 ) pad2YPos -= 10;
+    if(LKeyState && pad2YPos < window.innerHeight - 100 ) pad2YPos += 10;
+}
+canvas.onclick = () =>{
+    if(!requestFrame){
+        var ball = new Obj(docWidth/2, docHeight/2, 10)
+    ball.desenharBola();
+    requestFrame = true 
+    moverBolaLoop(ball)
+    }
 }
 
+
+
+
+
+function checarColisao(ballY, ballX){
+    ballX = ballX - 5;
+    let loclPad1xPos = 50 + 12.5
+    distance1 = Math.abs(ballX - loclPad1xPos)
+
+    if (distance1 < 5 && ballY > (pad1YPos - 50) && pad1YPos + 100 > ballY) dirX = true;
+    ballX +=10
+    let loclPad2xPos = docWidth - 50;
+    distance2 = Math.abs(ballX - loclPad2xPos)
+    if (distance2 < 5 && ballY > (pad1YPos - 50) && pad2YPos + 100 > ballY) dirX = false;
+
+}
+
+function aleatorio(){
+    return Boolean(Math.floor(Math.random() * 2))
+}
+
+canvasSetup()
